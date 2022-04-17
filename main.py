cee8296d3f8aff04b5646ed9ef7a8b7c0ebe0943
shayanekat <shaykatfsx@gmail.com>
2022-04-17 19:27:14 +0200

    add modules, and display layout

--- main.py
+++ main.py
@@ -5,17 +5,29 @@
 
 # ===== Imports =====
 import pygame
+from module.variables import *
 
 # ===== variables globales =====
-WIDTH = 800
-HEIGHT = 500
+
 
 # ===== fonctions =====
 def render():
     """
         Fonction pour l'affichage d'une frame
     """
-    pass
+    # reset l'écran
+    screen.fill((0, 0, 0))
+    
+    # le décor
+    pygame.draw.rect(screen, SkyColor, (0, 0, WIDTH, SkyHeight)) # le ciel
+    pygame.draw.rect(screen, GrassColor, (0, SkyHeight, WIDTH, HEIGHT)) # le sol
+    pygame.draw.polygon(screen, RoadColor, [(WIDTH//2, SkyHeight), (WIDTH//2 + NearRoadWidth//2, HEIGHT), (WIDTH//2 - NearRoadWidth//2, HEIGHT)]) # la route
+    
+    # test en dessinant les lignes de la route
+    pygame.draw.line(screen, (255, 255, 255), (WIDTH//2, SkyHeight), (WIDTH//2 - NearRoadWidth//2, HEIGHT), WhiteLineWidth)
+    pygame.draw.line(screen, (255, 255, 255), (WIDTH//2, SkyHeight), (WIDTH//2 + NearRoadWidth//2, HEIGHT), WhiteLineWidth)
+    
+    pygame.display.flip()
 
 # ===== frontend =====
 # initialisation de la fenêtre
@@ -29,6 +41,9 @@ running = True
 while running:
     # MàJ de l'écran
     pygame.display.flip()
+    
+    # partie principale
+    render()
     
     # boucle d'evenement
     for event in pygame.event.get():
---
+++ module/variables.py
@@ -0,0 +1,24 @@
+"""
+    Shayanekat
+    Pour simplifier le programme principal du jeu
+    je met toutes les variables globales dans ce module
+"""
+
+# taille de la fenêtre
+WIDTH = 800
+HEIGHT = 500
+
+# ciel
+SkyHeight = HEIGHT//4 # hauteur du ciel
+SkyColor = (0, 204, 255) # couleur du ciel
+
+# route
+NearRoadWidth = WIDTH//2 # largeur de la route proche
+WhiteLineLength = 20 # longueur de la ligne blanche
+WhiteLineWidth = 5 # largeur de la ligne blanche
+RoadColor = (82, 82, 87) # couleur de la route
+
+LaneN = 3 # le nombre de voies
+
+# autres couleurs
+GrassColor = (126, 200, 55) # couleur du sol (herbe)
